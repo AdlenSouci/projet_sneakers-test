6060bca7d3d4ca849eed20313d893bbb5989f1af
devtestpro <adlenssouci03@gmail.com>
2025-05-22 12:28:24 +0200

    new controller

--- routes/api.php
+++ routes/api.php
@@ -103,4 +103,63 @@ Route::put('/commandes/{id}', [CommandeController::class, 'update']);
 Route::delete('/commandes/{id}', [CommandeController::class, 'destroy']);
 
 
+//middleware route api pour les commandes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/commandes', [CommandeController::class, 'index']);
+    Route::post('/commandes', [CommandeController::class, 'store']);
+    Route::put('/commandes/{id}', [CommandeController::class, 'update']);
+    Route::delete('/commandes/{id}', [CommandeController::class, 'destroy']);
+});
+
+//middleware route api pour les annonces
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/annonces', [AnnonceController::class, 'index']);
+    Route::post('/annonces', [AnnonceController::class, 'store']);
+    Route::put('/annonces/{id}', [AnnonceController::class, 'update']);
+    Route::delete('/annonces/{id}', [AnnonceController::class, 'destroy']);
+});
+
+//middleware route api pour les articles
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/articles', [ArticleController::class, 'index']);
+    Route::post('/articles', [ArticleController::class, 'store']);
+    Route::put('/articles/{id}', [ArticleController::class, 'update']);
+    Route::delete('/articles/{id}', [ArticleController::class, 'destroy']);
+});
+
+//middleware route api pour les familles
+Route::middleware('auth:sanctum')->group(function () {
+    // Route::get('/familles', [FamilleController::class, 'index']);
+    Route::post('/familles', [FamilleController::class, 'store']);
+    Route::put('/familles/{id}', [FamilleController::class, 'update']);
+    Route::delete('/familles/{id}', [FamilleController::class, 'destroy']);
+});
+
+//middleware route api pour les marques
+Route::middleware('auth:sanctum')->group(function () {
+    // Route::get('/marques', [MarqueController::class, 'index']);
+    Route::post('/marques', [MarqueController::class, 'store']);
+    Route::put('/marques/{id}', [MarqueController::class, 'update']);
+    Route::delete('/marques/{id}', [MarqueController::class, 'destroy']);
+});
+
+//middleware route api pour les couleurs
+Route::middleware('auth:sanctum')->group(function () {
+    // Route::get('/couleurs', [CouleurController::class, 'index']);
+    Route::post('/couleurs', [CouleurController::class, 'store']);
+    Route::put('/couleurs/{id}', [CouleurController::class, 'update']);
+    Route::delete('/couleurs/{id}', [CouleurController::class, 'destroy']);
+}); 
+
+//middleware route api pour les users
+Route::middleware('auth:sanctum')->group(function () {
+    // Route::get('/users', [UserController::class, 'index']);
+    Route::post('/users', [UserController::class, 'store']);
+    Route::put('/users/{id}', [UserController::class, 'update']);
+    Route::delete('/users/{id}', [UserController::class, 'destroy']);
+});
+
+
+
+
 
